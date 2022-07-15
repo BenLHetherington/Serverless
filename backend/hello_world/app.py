276acab524cd9f438c4b4846646eed468967c8b5
BenLHetherington <Ben.L.Hetherington@gmail.com>
2022-07-15 16:45:42 -0600

    second

--- backend/hello_world/app.py
+++ backend/hello_world/app.py
@@ -3,7 +3,12 @@ import os
 import logging
 import boto3
 from botocore.exceptions import ClientError
-# import requests
+import pandas as pd
+import openpyxl
+import numpy as np
+import math
+from scipy.stats import norm
+#import norm
 
 # Handle logger
 logger = logging.getLogger()
@@ -23,18 +28,6 @@ else: # Running in AWS
     table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 logger.info("Finished conditional dynamodb logic")
-
-def returnError():
-    logger.error('Returning 500 status code')
-    return {
-            "statusCode": 500,
-            'headers': {
-                'Access-Control-Allow-Origin': os.environ['CORS_URL'],
-                'Access-Control-Allow-Credentials': 'true',
-                'Access-Control-Allow-Headers': 'Authorization',
-                'Content-Type': 'application/json'
-            }
-        }
 
 def getUserCount():
     try:
@@ -57,10 +50,8 @@ def getUserCount():
     except ClientError as e:
         if e.response['Error']['Code'] == 'RequestLimitExceeded':
             logger.error('ERROR: ', e)
-            returnError()
         else:
             logger.error("UNEXPECTED ERROR from DDB: %s" % e)
-            returnError()
 
 def updateUserCount(count):
     try:
@@ -80,45 +71,151 @@ def updateUserCount(count):
         else:
             logger.error("UNEXPECTED ERROR from DDB: %s" % e)
 
-# def get_github_start():
-#     # response = requests.get('https://api.github.com/repos/chrishart0/gsd-aws-cdk-serverless-example')
-#     # stared_count = response.content['stargazers_count']
-#     # print("resp:")
-#     # print(stared_count)
-#     return 5
+
+#Function for AWS that does all the math
+def genMat(RID, MVAL, BlockSize):
+    
+    #Reads Data In
+    dataSheet = pd.read_excel(r'dataDLOM.xlsx', sheet_name='Sheet1')
+    dataSheetColumnNames = dataSheet.columns.tolist()
+    dataSheet=dataSheet.to_numpy()
+    dataSheetLength = dataSheet.shape[0]
+    dataSheetWidth = dataSheet.shape[1]
+
+    #Declares a few variables as set up
+    DISCAASPMat = []
+    DISCBSPMat = []
+    DISCLBPMat = []
+    tFactor = math.log(100) - math.log(100-BlockSize)
+    display = 0
+
+    #Find row where RID == ymrt
+    i=0
+    while i < dataSheetLength:
+        if dataSheet[i][0] == RID:
+            # If MVAL is not within bounds then break
+            if MVAL > dataSheet[i][3]:
+                logger.info("MVAL is too high. Please change to fit inside bounds ")
+                # print('MVAL is too high. Please change to fit inside bounds ', dataSheet[i][2],' - ',dataSheet[i+9][3])
+            elif MVAL < dataSheet[i+9][2]:
+                logger.info("MVAL is too low. Please change to fit inside bounds ")
+                # print('MVAL is too low. Please change to fit inside bounds ', dataSheet[i][2],' - ',dataSheet[i][3])
+            else:
+                logger.info("Date is: "+str(RID))
+                logger.info("MVAL is: "+str(MVAL))
+                logger.info("Block Size is: "+str(BlockSize))
+                i = findMVALIndex(i, dataSheet)
+                display = 1
+                t1 = tFactor*dataSheet[i][8]   #h-life Mean * tFactor
+                sigma = dataSheet[i][4] #Sig Mean
+                sigsqt = t1*(sigma)**2
+                sigsqrtt = math.sqrt(sigsqt)
+                d1 = sigsqrtt/(4*math.sqrt(3))
+                d2 = -3*d1
+                v1 = math.exp(sigsqt)
+                v2 = sigsqt + (math.log(2 * (v1 - sigsqt - 1))) - (2 * math.log(v1 - 1))
+                v3 = math.sqrt(v2 / t1)
+                v4 = v3 * math.sqrt(t1)
+
+                EXAASP = 2 * norm.cdf(v4 / 2)
+                EXBSP = 2 * norm.cdf(sigsqrtt / 2)
+                EXLBP = (2+sigsqt/2)* norm.cdf(sigsqrtt/2)+(math.sqrt(sigsqt*7/44))*(math.exp(-sigsqt/8))
+
+                DISCAASPMean = (EXAASP - 1) / EXAASP
+                DISCBSPMean = (EXBSP - 1) / EXBSP
+                DISCLBPMean = (EXLBP - 1) / EXLBP
+
+                ## Looping the 3x3 array
+                SIGArray = [dataSheet[i][5], dataSheet[i][6], dataSheet[i][7]]
+                hlifeArray = [dataSheet[i][9], dataSheet[i][10], dataSheet[i][11]]
+                for x in SIGArray:
+                    for y in hlifeArray:
+                        t1 = tFactor*y  # h-life * tFactor
+                        sigma = x  # Sig
+                        sigsqt = t1*(sigma)**2
+                        sigsqrtt = math.sqrt(sigsqt)
+                        d1 = sigsqrtt / (4 * math.sqrt(3))
+                        d2 = -3 * d1
+                        v1 = math.exp(sigsqt)
+                        v2 = sigsqt + (math.log(2 * (v1 - sigsqt - 1))) - (2 * math.log(v1 - 1))
+                        v3 = math.sqrt(v2 / t1)
+                        v4 = v3 * math.sqrt(t1)
+
+                        EXAASP = 2 * norm.cdf(v4 / 2)
+                        EXBSP = 2 * norm.cdf(sigsqrtt / 2)
+                        EXLBP = (2 + sigsqt / 2) * norm.cdf(sigsqrtt / 2) + (math.sqrt(sigsqt * 7 / 44)) * (
+                            math.exp(-sigsqt / 8))
+
+                        DISCAASP = (EXAASP - 1) / EXAASP
+                        DISCBSP = (EXBSP - 1) / EXBSP
+                        DISCLBP = (EXLBP - 1) / EXLBP
+
+                        DISCAASPMat.append(DISCAASP)
+                        DISCBSPMat.append(DISCBSP)
+                        DISCLBPMat.append(DISCLBP)
+
+            break
+        i=i+1
+
+    if display == 1 :
+        DISCAASPMat = np.reshape(DISCAASPMat, (-1, 3))
+        DISCBSPMat = np.reshape(DISCBSPMat, (-1, 3))
+        DISCLBPMat = np.reshape(DISCLBPMat, (-1, 3))
+
+    return DISCAASPMat, DISCBSPMat, DISCLBPMat, DISCAASPMean, DISCBSPMean, DISCLBPMean;
+
+def extract_child_from_body_of_apg_event(event, child_item, mandatory): 
+    try:
+        passed_value = event['multiValueQueryStringParameters'][child_item][0]
+        return passed_value
+    except (KeyError, json.decoder.JSONDecodeError, TypeError) as e: #If passed value is empty then throw an error
+        if(mandatory):
+            logger.error(f"Could not find value for: {child_item}")
+            raise 'ERROR: Must pass in all required values!'
 
 def lambda_handler(event, context):
-
-    logger.info("Lambda handler invocation initiated")
+    RID=extract_child_from_body_of_apg_event(event, 'RID', mandatory=True)
+    MVAL=extract_child_from_body_of_apg_event(event, 'MVAL', mandatory=True)
+    BlockSize=extract_child_from_body_of_apg_event(event, 'BlockSize', mandatory=True)
+    RID=RID.replace('-','')
+    RID=int(RID)
+    BlockSize=int(BlockSize)
+    MVAL=int(MVAL)
+    logger.info("RID: "+str(RID))
+    logger.info("BlockSize: "+str(BlockSize))
+    logger.info("MVAL: "+str(MVAL))
+    logger.info(type(MVAL))
 
     user_count = getUserCount()
     updateUserCount(user_count)
+    DISCAASPMat, DISCBSPMat, DISCLBPMat, DISCAASPMean, DISCBSPMean, DISCLBPMean = genMat(RID, MVAL, BlockSize)
+    logger.info("DISCAASPMean: "+str(DISCAASPMean))
+    logger.info("DISCBSPMean: "+str(DISCBSPMean))
+    logger.info("DISCLBPMean: "+str(DISCLBPMean))
+    logger.info("User count: "+str(user_count))
+    DISCAASPMat = DISCAASPMat.tolist()
+    logger.info(DISCAASPMat)
+    DISCBSPMat = DISCBSPMat.tolist()
+    logger.info(DISCBSPMat)
+    DISCLBPMat = DISCLBPMat.tolist()
+    logger.info(DISCLBPMat)
 
-    # gitHub_stars = get_github_start()
 
-    if not (user_count):
-        logger.error('Something went wrong, returning 500')
-        return {
-        "statusCode": 500,
-        'headers': {
-            'Access-Control-Allow-Origin': os.environ['CORS_URL'],
-            'Access-Control-Allow-Credentials': 'true',
-            'Access-Control-Allow-Headers': 'Authorization',
-            'Content-Type': 'application/json'
-        }
-    }
-
-    logger.info('Function completed successfully, returning 200')
     return {
         "statusCode": 200,
-        'headers': {
-            'Access-Control-Allow-Origin': os.environ['CORS_URL'],
-            'Access-Control-Allow-Credentials': 'true',
-            'Access-Control-Allow-Headers': 'Authorization',
-            'Content-Type': 'application/json'
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            "Content-Type": "application/json"
         },
         "body": json.dumps({
             "User count": str(user_count),
-            # "GitHub stars": str(gitHub_stars)
+            "DISCAASPMat": DISCAASPMat,
+            "DISCBSPMat": DISCBSPMat,
+            "DISCLBPMat": DISCLBPMat,
+            "DISCAASPMean": str(DISCAASPMean),
+            "DISCBSPMean": str(DISCBSPMean),
+            "DISCLBPMean": str(DISCLBPMean)
         }),
     }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,89 +1,168 @@
-import React, { useEffect, useState } from "react";
-import "@fontsource/roboto";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import './App.css'; 
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 
-// Components
-import Navbar from "components/navBar";
-
-// Assets
-import githubLogo from "images/githubLogo.png";
-import infraDiagram from "images/Infra-Diagram.drawio.png";
-
-// CSS / Themeing
-import "./App.css";
-import CssBaseline from "@mui/material/CssBaseline";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-// Define theme settings
-const lightMode = {
-  palette: {
-    mode: "light",
-  },
-};
-
-// https://mui.com/customization/dark-mode/
-const darkMode = {
-  palette: {
-    mode: "dark",
-  },
-};
+import githubLogo from 'images/githubLogo.png'
 
 function App() {
   const [userCount, setUserCount] = useState(false);
-  // The light theme is used by default
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [BlockSize, setBlockSize] = useState(false);
+  const [RID, setRID] = useState(false);
+  const [MVAL, setMVAL] = useState(false);
+  const [DISCAASPMat, setDISCAASPMat] = useState(false);
+  const [DISCBSPMat, setDISCBSPMat] = useState(false);
+  const [DISCLBPMat, setDISCLBPMat] = useState(false);
+  const [DISCAASPMean, setDISCAASPMean] = useState(false);
+  const [DISCBSPMean, setDISCBSPMean] = useState(false);
+  const [DISCLBPMean, setDISCLBPMean] = useState(false);
+  // var [PassiveTable, setPassiveTable] = useState(false);
 
   //Special handling to use localhost SAM API if running locally via npm start(make run)
-  const apiUrl =
-    process.env.NODE_ENV !== "development"
-      ? "https://api." + process.env.REACT_APP_DOMAIN + "/users"
-      : process.env.REACT_APP_USER_API_URL_LOCAL_SAM;
+  const apiUrl = (process.env.NODE_ENV !== 'development') ? 'https://' + process.env.REACT_APP_USER_API_DOMAIN + '/users' : process.env.REACT_APP_USER_API_URL_LOCAL_SAM
 
-  async function fetchUserCount() {
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        setUserCount(response["User count"]);
+
+  // function GenerateTable() {
+  //   // Gate to catch if all params not filled
+  //   if (!(factors)) {
+  //     return null;
+  //   }
+  //   if (TotalSalesGrowth < 0) {
+  //     return (
+  //       <Typography variant='h5'>No further analysis as there is no positive growth to apportion between active and passive components</Typography>
+  //     );
+  //   }
+
+  //   PassiveTable = [];
+  //   for (let i = 0; i < Object.keys(factors).length; i++) {
+  //     PassiveTable[i]={
+  //       "Factor": factors[i][1],
+  //       "BeginningValue": parseFloat(factors[i][5]),
+  //       "EndingValue": parseFloat(factors[i][6]),
+  //       "TotalGrowth": parseFloat(factors[i][3]*100),
+  //       "Elasticity": parseFloat(factors[i][2]),
+  //       "Impact": parseFloat(factors[i][4])
+  //     };
+  //   };
+  //   console.log('PassiveTable: ', PassiveTable);
+  //   // return (PassiveTable);
+  //   return (
+  //     <div className="App">
+  //       <Typography >The passive appreciation estimate presented here is based on the data for a diversified collection of all businesses, nationwide, for each retail sector, The passive appreciation proportion for individual businesses may differ depending on the demographic and economic conditions specific to their area of operation. </Typography>
+  //       <br/>
+  //       <table>
+  //         <tr>
+  //           <th>Factor Name</th>
+  //           <th>Beginning Value</th>
+  //           <th>Ending Value</th>
+  //           <th>Total Growth</th>
+  //           <th>Elasticity</th>
+  //           <th>Impact</th>
+  //         </tr>
+  //         {PassiveTable.map((val, key) => {
+  //           return (
+  //             <tr key={key}>
+  //               <td>{val.Factor}</td>
+  //               <td>{val.BeginningValue.toFixed(3)}</td>
+  //               <td>{val.EndingValue.toFixed(3)}</td>
+  //               <td>{val.TotalGrowth.toFixed(2)} %</td>
+  //               <td>{val.Elasticity.toFixed(5)}</td>
+  //               <td>{val.Impact.toFixed(6)}</td>
+  //             </tr>
+  //           )
+  //         })}
+  //       </table>
+  //       {/* <br/>
+  //       <Typography variant='h5'>Total Influencer Economic Factor Impact: {parseFloat(InfluencerEconomicFactorImpact*100).toFixed(2)} %</Typography>
+  //       <Typography variant='h5'>Passive Growth: {parseFloat(passiveGrowthVar*100).toFixed(2)} %</Typography>
+  //       <br/> */}
+  //     </div>
+  //   );
+  // }
+
+
+  // Prevent continuous reloading calling API each time
+  // This makes the call to the backend
+  useEffect(() => {
+    if (BlockSize && RID && MVAL){
+      //Variables being sent
+      fetch(apiUrl+"?BlockSize="+BlockSize+"&RID="+RID+"&MVAL="+MVAL)
+      .then(response => response.json())
+      //Response variables
+      .then(response => {
+        console.log(response)
+        setUserCount(response['User count'])
+        setDISCAASPMat(response['DISCAASPMat'])
+        setDISCBSPMat(response['DISCBSPMat'])
+        setDISCLBPMat(response['DISCLBPMat'])
+        setDISCAASPMean(response['DISCAASPMean'])
+        setDISCBSPMean(response['DISCBSPMean'])
+        setDISCLBPMean(response['DISCLBPMean'])
+        //Console logging for debugging
+        console.log('Length of DISCAASPMat Array: ', Object.keys(DISCAASPMat).length)
+        console.log('Length of DISCBSPMat Array: ', Object.keys(DISCBSPMat).length)
+        console.log('Length of DISCLBPMat Array: ', Object.keys(DISCLBPMat).length)
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
-  }
-
-  //Prevent continuous reloading calling API each time
-  useEffect(() => {
-    fetchUserCount();
-  }, []);
-
+    }
+  }, [BlockSize, RID, MVAL] );
+  
   return (
-    <ThemeProvider theme={isDarkTheme ? createTheme(darkMode) : createTheme(lightMode)}>
-      <CssBaseline />
-      <div className="App">
-        <Navbar className="AppBar" isDarkTheme={isDarkTheme} setIsDarkTheme={setIsDarkTheme} />
-
-        <Container className="body" maxWidth="md">
-          <Typography variant="h2">Welcome to this demo site!</Typography>
-          <Typography variant="h5">Made with the S3, Lambda, and DDB stack</Typography>
-          <br />
-          <Typography>
-            This demo shows how to use a static site hosted in S3, a Lambda function + APG, and DynamoDB. All testing locally and in the
-            CI/CD.
+    <div className="App">
+      <header className="App-header">
+      <br/>
+        <Container className='header' maxWidth='md'>
+          <Typography variant='h2'>
+            DLOM Calculator
           </Typography>
-          <br />
-          <Typography>
-            <a href="https://github.com/chrishart0/gsd-aws-cdk-serverless-example" target="_blank" rel="noreferrer">
-              <img height={20} src={githubLogo} alt="Logo" />
-              Edit on Github!
-            </a>
+          <Typography variant='h5'>
+          Dr. Ashok Abbott
           </Typography>
-          <Typography className="visitorCounter">Visitor Count: {userCount}</Typography>
-          <Typography variant="h3">Infrastructure Diagram</Typography>
-          <img width={"80%"} src={infraDiagram} alt="Infra Diagram" />
+          <br/>
+          <Typography variant='h5'>
+          User Date: 
+          </Typography>
+          <form>
+            <input type="month" id="start" name="start"
+              min="1995-01" max="2021-12" onChange={RID => setRID(RID.target.value)} ></input>
+          </form>
+          <Typography variant='h5'>
+          MVAL: 
+          </Typography>
+          <input type="number" 
+            onChange={MVAL => setMVAL(MVAL.target.value)} ></input>
+          <Typography variant='h5'>
+          Block Size: 
+          </Typography>
+          <Typography>
+          As a percentage between 0 and 100
+          </Typography>
+          <input type="number" min={0} max={100}
+            onChange={BlockSize => setBlockSize(BlockSize.target.value)} ></input>
+          <br/>
+          <br/>
+          <Typography>
+          This demo shows PLACEHOLDER
+          </Typography>
+          <br/>
+          <Typography variant='h6'>DISCAASPMean is {DISCAASPMean}</Typography>
+          <Typography variant='h6'>DISCBSPMean is {DISCBSPMean}</Typography>
+          <Typography variant='h6'>DISCLBPMean is {DISCLBPMean}</Typography>
+          <br/>
+          {/* {GenerateTable()} */}
+          {/* {PrintTable()} */}
+          {/* {pasiveGrowthPrintout()} */}
+          {/* {FactorGrowthCheck()} */}
+          <br/>
+          <Typography className='visitorCounter'>Please contact "ashok.abbott@bizvalinc.com" with any questions/comments/suggestions</Typography>
+          <Typography className='visitorCounter'>Visitor Count: {userCount}</Typography>
         </Container>
-      </div>
-    </ThemeProvider>
+      </header>
+    </div>
   );
 }
 
